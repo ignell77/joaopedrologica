8bb78f4a5cc144ddac0fea3963bac94c1ee2f152
ignell77 <50590879+ignell77@users.noreply.github.com>
2019-05-13 14:18:17 -0300

    dados completo

--- LOGICA DE PROGRAMAÇÃO JP PYTHON/DADOS.py
+++ LOGICA DE PROGRAMAÇÃO JP PYTHON/DADOS.py
@@ -1,19 +1,34 @@
-# Meu primeiro programa 
+# Meu primeiro programa
 
 from random import randint  # importa a função de gerar números aleatórios
 
-print("olá 1info2!")
+print("Jogos dos dados: ")
 
-dado1 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
+dado1 = randint(1, 6)   # gera um número aleatório entre 1 e 6
 print("Dado 1: ", dado1)
 
-dado2 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
-print("Dado 2: ", dado1)
+dado2 = randint(1, 6)   # gera um número aleatório entre 1 e 6
+print("Dado 2: ", dado2)
 
 
-dado3 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
-print("Dado 3: ", dado1)
+dado3 = randint(1, 6)   # gera um número aleatório entre 1 e 6
+print("Dado 3: ", dado3)
 
 
-dado4 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
-print("Dado 4: ", dado1)
+dado4 = randint(1, 6)   # gera um número aleatório entre 1 e 6
+print("Dado 4: ", dado4)
+
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print('jogador 1: ', jogador1)
+print('jogador 2: ', jogador2)
+
+if jogador1 > jogador2:
+    print("jogador um venceu!")
+else:
+    if jogador2 > jogador1:
+        print("jogador2 venceu!")
+    else:
+        print("Empate!")
