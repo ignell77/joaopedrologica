3ef852a2778bbc697df9017eb6f6e18852a5f854
ignell77 <50590879+ignell77@users.noreply.github.com>
2019-05-13 13:42:44 -0300

    pasta P Dados incluindo os numeros

---
+++ LOGICA DE PROGRAMAÇÃO JP PYTHON/DADOS.py
@@ -0,0 +1,19 @@
+# Meu primeiro programa 
+
+from random import randint  # importa a função de gerar números aleatórios
+
+print("olá 1info2!")
+
+dado1 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
+print("Dado 1: ", dado1)
+
+dado2 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
+print("Dado 2: ", dado1)
+
+
+dado3 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
+print("Dado 3: ", dado1)
+
+
+dado4 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
+print("Dado 4: ", dado1)
