98983c1f0e0ec66091aac66422a6c0303a7f87db
ignell77 <50590879+ignell77@users.noreply.github.com>
2019-08-27 08:37:21 -0300

    Delete DADOS.py

--- DADOS.py
+++
@@ -1,8 +0,0 @@
-# Meu primeiro programa 
-
-from random import randint  # importa a função de gerar números aleatórios
-
-print("olá 1info2!")
-
-dado1 = randint( 1 , 6)   # gera um número aleatório entre 1 e 6 
-print("Dado 1: ", dado1)
